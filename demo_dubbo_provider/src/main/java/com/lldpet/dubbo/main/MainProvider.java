62d33dcc51ea7560cc58a8e50d90bc27af56d452
llduo <xglguan@163.com>
2016-11-18 12:33:08 +0800

    delete

--- demo_dubbo_provider/src/main/java/com/lldpet/dubbo/main/MainProvider.java
+++ demo_dubbo_provider/src/main/java/com/lldpet/dubbo/main/MainProvider.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainProvider {
 	public static void main(String[] args) throws IOException {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"applicationProvider.xml"});
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
 		context.start();
 		System.out.println("按任意键退出");
 		System.in.read();
